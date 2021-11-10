372acf2702e340d09a48fbdfbaca886ca6e1fd50
Pilandma2018 <93951814+Pilandma2018@users.noreply.github.com>
2021-11-09 17:15:12 -0800

    Add files via upload
    Runs and works properly

--- Diving/src/main/java/LYT/Diving/Dive.java
+++ Diving/src/main/java/LYT/Diving/Dive.java
@@ -1,55 +1,94 @@
 package LYT.Diving;
 import java.util.*;
+import java.util.ArrayList;
 public class Dive {
-static void inputValidScore() {
-	float score;
-Scanner sc = new Scanner(System.in);
+	
+static Scanner sc = new Scanner(System.in);
+
+
+ public static double inputValidScore() {
+	double score=0.0;
+
 System.out.println("Input score from 1 judge: ");
-score= sc.nextFloat();
-System.out.println("Valid score is: " + score);
+score= sc.nextDouble();
+System.out.println("Score is: " + score);
+
+return score;
 }	
 
-public static void inputAllScores() {
-Scanner sc = new Scanner(System.in);	
-	
-	System.out.print("Enter the number of judges: ");
-	int i=sc.nextInt();
-	float[] scores = new float[i];
-	int[] array = new int[i];
-	
-	System.out.println("Enter the scores: ");
-	for(int j=0; j<i; j++)
-		{
-		scores[j] = sc.nextFloat();
-		}
-	
-System.out.println("List of scores: ");
+public static void inputAllScores(ArrayList<Double> score) {	
 
-	for (int j=0; j<i; j++)
-	{
-		System.out.println(scores[j]);
-return;
+	for(int j = 0; j < 7; j++) {
+		
+
+	double input = sc.nextDouble();
+	score.add(input);
+	}
+	}
+
+public static int highestNumber(ArrayList<Double> score) {
+	double high = score.get(0);
+	int i = 0;
+	int highest = 0;
+	for (i = 0; i < 7; i++) {
+		if (score.get(i) > high) {
+			high = score.get(i);
+			highest = i;
+		}
+	}
+	return highest; 
+}
+
+public static int lowestNumber(ArrayList<Double> score) {
+	double low = score.get(0);
+	int i = 0;
+	int lowest = 0;
+	for (i = 0; i < 7; i++) {
+		if (score.get(i) > low) {
+			low = score.get(i);
+			lowest = i;
+		}
+	}
+	return lowest; 
+}
+public static double addScore(ArrayList<Double> score, int highest, int lowest) {
+	
+	int i = 0;
+	score.remove(highest);
+	score.remove(lowest);
+	double sum = 0;
+	for (i = 0; i < score.size(); i ++) {
+		sum += score.get(i);
+		
+	}
+	return sum ;
 	
 }
-}
-static void inputValidDegreeOfDifficulty() {
+
+public static void main( String[] args) {
+		inputValidScore();
+		System.out.println("Enter the 7 scores: ");
+		
+	ArrayList<Double> score = new ArrayList<Double>();
+	inputAllScores(score);
+	System.out.println("Enter the difficulty between 1.2 and 3.8");
 	
-	float degree;
-Scanner sc = new Scanner(System.in);
+	double difficulty = sc.nextDouble();
+	
+	int indexhigh = highestNumber(score);
+	int indexlow = lowestNumber(score);
+	double sum = addScore(score, indexhigh, indexlow);
+	System.out.println("The total diver's score is : " + ((sum * difficulty) * 0.6));
+}
+
+/*public static double inputValidDegreeOfDifficulty(double degree) {
+
 System.out.println("Input degree of difficulty: ");
 degree= sc.nextFloat();
-System.out.println("Degree of difficulty is: " + degree);
-}	
 
-static void calculateScore() {
-
+return degree;
 }
+*/	
 
-	public static void main( String[] args) {
-inputValidScore();
-inputAllScores();
-inputValidDegreeOfDifficulty();	
-	
-}
 
 }
